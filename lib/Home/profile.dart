034e42e9c470ce737a79fcb2c2432098c4c4f7da
Uttam Kalaria <74789656+Uttam-Patel@users.noreply.github.com>
2023-02-09 18:36:33 +0530

    Profile page image edit option

--- lib/Home/profile.dart
+++ lib/Home/profile.dart
@@ -8,6 +8,7 @@ import 'package:image_picker/image_picker.dart';
 
 import '../login/login_screen.dart';
 import '../util/constants.dart';
+import '../util/drawer_menu.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -29,168 +30,242 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            //Profile Image
-            Center(
-              child: FutureBuilder(
-                future:
-                    FirebaseFirestore.instance.collection(uType).doc(uId).get(),
-                builder: (context, snapshot) {
-                  // Check for errors
-                  if (snapshot.hasError) {
-                    return Container();
-                  }
+      drawer: const MenuDrawer(),
+      appBar: AppBar(
+        title: const Text('LDRP-ITR'),
+        centerTitle: true,
+        actions: [
+          PopupMenuButton(
+            position: PopupMenuPosition.under,
+            itemBuilder: ((context) {
+              return [
+                PopupMenuItem(
+                  child: const Text('Refresh'),
+                  onTap: () {},
+                ),
+                PopupMenuItem(
+                  child: const Text('Report'),
+                  onTap: () {},
+                ),
+              ];
+            }),
+          ),
+        ],
+      ),
+      body: RefreshIndicator(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //Profile Image
+              Center(
+                child: StreamBuilder(
+                  stream: FirebaseFirestore.instance
+                      .collection(uType)
+                      .doc(uId)
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    // Check for errors
+                    if (snapshot.hasError) {
+                      return const Placeholder();
+                    }
 
-                  // Once complete, show your application
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    return profileImage(context);
-                  }
+                    // Once complete, show your application
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const CircleAvatar(
+                        radius: 80,
+                        child: CircularProgressIndicator(),
+                      );
+                    } else {
+                      return Stack(
+                        children: [
+                          CircleAvatar(
+                            radius: 80,
+                            backgroundImage:
+                                NetworkImage(snapshot.data!.data()!['photo']),
+                          ),
+                          Positioned(
+                            bottom: 20,
+                            right: 10,
+                            child: InkWell(
+                              onTap: () async {
+                                await showAlert(context);
+                                setState(() {});
+                              },
+                              child: const Icon(
+                                Icons.photo_camera,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    }
 
-                  // Otherwise, show something whilst waiting for initialization to complete
-                  return processIndicator();
-                },
+                    // Otherwise, show something whilst waiting for initialization to complete
+                  },
+                ),
               ),
-            ),
 
-            //Student Profile Page Fields
-            Visibility(
-              visible: userType == 'Student', //visible if usertype is 'Student'
-              child: Column(
-                children: [
-                  addProfileField('Name', '$lName $fName $mName', false),
-                  addProfileField('Enrollment Number', userId, false),
-                  addProfileField('Email Id', email, false),
-                  addProfileField('Phone Number', phone, false),
-                  addProfileField('Account Type', accountType, false),
-                  addProfileField('Cource', course, false),
-                  addProfileField('Year', year, false),
-                  addProfileField('Semester', semester, false),
-                ],
+              //Student Profile Page Fields
+              Visibility(
+                visible:
+                    userType == 'Student', //visible if usertype is 'Student'
+                child: Column(
+                  children: [
+                    addProfileField('Name', '$lName $fName $mName', false),
+                    addProfileField('Enrollment Number', userId, false),
+                    addProfileField('Email Id', email, false),
+                    addProfileField('Phone Number', phone, false),
+                    addProfileField('Account Type', accountType, false),
+                    addProfileField('Cource', course, false),
+                    addProfileField('Year', year, false),
+                    addProfileField('Semester', semester, false),
+                  ],
+                ),
               ),
-            ),
-            //Teacher Profile Page Fields
-            Visibility(
-              visible: userType == 'Teacher', //visible if usertype is 'Teacher'
-              child: Column(
-                children: [
-                  addProfileField('Name', '$lName $fName', false),
-                  addProfileField('Teacher Id', userId, false),
-                  addProfileField('Phone Number', phone, false),
-                  addProfileField('Account Type', accountType, false),
-                  addProfileField('Department', department, false),
-                ],
+              //Teacher Profile Page Fields
+              Visibility(
+                visible:
+                    userType == 'Teacher', //visible if usertype is 'Teacher'
+                child: Column(
+                  children: [
+                    addProfileField('Name', '$lName $fName', false),
+                    addProfileField('Teacher Id', userId, false),
+                    addProfileField('Phone Number', phone, false),
+                    addProfileField('Account Type', accountType, false),
+                    addProfileField('Department', department, false),
+                  ],
+                ),
               ),
-            ),
-            Visibility(
-              visible: userType == 'Admin', //visible if usertype is 'Teacher'
-              child: Column(
-                children: [
-                  addProfileField('Name', '$lName $fName', false),
-                  addProfileField('Admin Id', userId, false),
-                  addProfileField('Phone Number', phone, false),
-                  addProfileField('Account Type', accountType, false),
-                ],
+              Visibility(
+                visible: userType == 'Admin', //visible if usertype is 'Teacher'
+                child: Column(
+                  children: [
+                    addProfileField('Name', '$lName $fName', false),
+                    addProfileField('Admin Id', userId, false),
+                    addProfileField('Phone Number', phone, false),
+                    addProfileField('Account Type', accountType, false),
+                  ],
+                ),
               ),
-            ),
 
-            //Sign Out Button
-            Center(
-              child: ElevatedButton.icon(
-                label: Text('Sign Out'),
-                icon: Icon(Icons.logout),
-                onPressed: () async {
-                  await FirebaseAuth.instance.signOut().then((value) async {
-                    Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(builder: (context) => LogIn()),
-                        (route) => false);
-                    await removeLocalData();
-                  });
-                },
-                style: ButtonStyle(
-                    fixedSize: MaterialStateProperty.all(Size(200, 50))),
-              ),
-            )
-          ],
+              //Sign Out Button
+              Center(
+                child: ElevatedButton.icon(
+                  label: Text('Sign Out'),
+                  icon: Icon(Icons.logout),
+                  onPressed: () async {
+                    await FirebaseAuth.instance.signOut().then((value) async {
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(builder: (context) => LogIn()),
+                          (route) => false);
+                      await removeLocalData();
+                    });
+                  },
+                  style: ButtonStyle(
+                      fixedSize: MaterialStateProperty.all(Size(200, 50))),
+                ),
+              )
+            ],
+          ),
         ),
+        onRefresh: () async {
+          await FirebaseFirestore.instance
+              .collection(uType)
+              .doc(uId)
+              .get()
+              .then((value) {
+            setState(() {
+              photo = value['photo'];
+            });
+          });
+        },
       ),
     );
   }
 
   //Edit image alert box code
-  AlertDialog showAlert(context) {
-    return AlertDialog(
-      title: const Text(
-        "Choose Profile Photo",
-      ),
-      content: Container(
-        height: (this._imageFile == null) ? 80 : 170,
-        // margin: EdgeInsets.symmetric(horizontal: 20,vertical: 20),
-        child: Column(
-          children: [
-            (this._imageFile != null) ? pickedImage() : chooseUploadOption()
-          ],
-        ),
-      ),
-      actions: <Widget>[
-        TextButton(
-          child: const Text('Cancel'),
-          onPressed: () {
-            Navigator.of(context).pop();
-            _clearCatchImage();
-          },
-        ),
-        Visibility(
-          visible: this._imageFile != null,
-          child: TextButton(
-            child: const Text('Upload'),
-            onPressed: () async {
-              await _uploadImageToFirebaseStorage();
-              await _updateProfileImage();
-              _clearCatchImage();
-              Navigator.of(context).pop();
-            },
-          ),
-        ),
-      ],
-    );
-  }
-
-  //Profile Image function code
-  CircleAvatar processIndicator() {
-    return CircleAvatar(
-      radius: 80,
-      child: CircularProgressIndicator(),
-    );
-  }
-
-  Stack profileImage(context) {
-    return Stack(
-      children: [
-        CircleAvatar(
-          radius: 80,
-          backgroundImage: NetworkImage(photo),
-        ),
-        Positioned(
-          bottom: 20,
-          right: 10,
-          child: InkWell(
-            onTap: () {
-              showDialog(
-                  context: context,
-                  builder: (BuildContext context) => showAlert(context));
-            },
-            child: const Icon(
-              Icons.photo_camera,
-              color: Colors.black,
-            ),
-          ),
-        ),
-      ],
-    );
+  Future showAlert(context) async {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) => StatefulBuilder(
+            builder: (context, setState) => AlertDialog(
+                  title: const Text(
+                    "Choose Profile Photo",
+                  ),
+                  content: Container(
+                    height: (this._imageFile != null) ? 170 : 80,
+                    // margin: EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+                    child: (this._imageFile != null)
+                        ? CircleAvatar(
+                            radius: 80,
+                            backgroundImage: FileImage(this._imageFile!),
+                          )
+                        : Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Column(
+                                children: [
+                                  IconButton(
+                                    padding: EdgeInsets.all(0),
+                                    icon: const Icon(
+                                      Icons.photo_camera_outlined,
+                                      size: 50,
+                                    ),
+                                    onPressed: () async {
+                                      await _pickImageFromCamera();
+                                      setState(() {});
+                                    },
+                                    tooltip: 'Shoot picture',
+                                  ),
+                                  const Text('Camera'),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  IconButton(
+                                    padding: EdgeInsets.all(0),
+                                    icon: const Icon(
+                                      Icons.photo_outlined,
+                                      size: 50,
+                                    ),
+                                    onPressed: () async {
+                                      await _pickImageFromGallery();
+                                      setState(() {});
+                                    },
+                                    tooltip: 'Pick from gallery',
+                                  ),
+                                  const Text('Gallery'),
+                                ],
+                              ),
+                            ],
+                          ),
+                  ),
+                  actions: <Widget>[
+                    TextButton(
+                      child: const Text('Cancel'),
+                      onPressed: () async {
+                        Navigator.of(context).pop();
+                        _clearCatchImage();
+                      },
+                    ),
+                    Visibility(
+                      visible: this._imageFile != null,
+                      child: TextButton(
+                        child: const Text('Upload'),
+                        onPressed: () async {
+                          await _uploadImageToFirebaseStorage().then(
+                            (value) {
+                              Navigator.of(context).pop();
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                )));
   }
 
 //Profile Page Build
@@ -222,7 +297,7 @@ class _ProfileState extends State<Profile> {
   Future<void> _pickImageFromGallery() async {
     final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
-      setState(() {
+      this.setState(() {
         this._imageFile = File(pickedFile.path);
       });
     }
@@ -231,7 +306,7 @@ class _ProfileState extends State<Profile> {
   Future<void> _pickImageFromCamera() async {
     final pickedFile = await _picker.pickImage(source: ImageSource.camera);
     if (pickedFile != null) {
-      setState(() {
+      this.setState(() {
         this._imageFile = File(pickedFile.path);
       });
     }
@@ -244,7 +319,9 @@ class _ProfileState extends State<Profile> {
 
     try {
       if (this._imageFile != null) {
-        storageRef.putFile(this._imageFile!);
+        storageRef.putFile(this._imageFile!).whenComplete(() async {
+          await _updateProfileImage();
+        });
       }
     } on FirebaseException catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
@@ -259,7 +336,9 @@ class _ProfileState extends State<Profile> {
       await FirebaseFirestore.instance
           .collection(uType)
           .doc(uId)
-          .update({'photo': url}).catchError((error) => print(error));
+          .update({'photo': url})
+          .then((value) => _clearCatchImage())
+          .catchError((error) => print(error));
     }
   }
 
@@ -275,48 +354,5 @@ class _ProfileState extends State<Profile> {
     setState(() {
       this._imageFile = null;
     });
-  }
-
-  Row chooseUploadOption() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        Column(
-          children: [
-            IconButton(
-              padding: EdgeInsets.all(0),
-              icon: const Icon(
-                Icons.photo_camera_outlined,
-                size: 50,
-              ),
-              onPressed: () async => _pickImageFromCamera(),
-              tooltip: 'Shoot picture',
-            ),
-            const Text('Camera'),
-          ],
-        ),
-        Column(
-          children: [
-            IconButton(
-              padding: EdgeInsets.all(0),
-              icon: const Icon(
-                Icons.photo_outlined,
-                size: 50,
-              ),
-              onPressed: () async => _pickImageFromGallery(),
-              tooltip: 'Pick from gallery',
-            ),
-            const Text('Gallery'),
-          ],
-        ),
-      ],
-    );
-  }
-
-  CircleAvatar pickedImage() {
-    return CircleAvatar(
-      radius: 80,
-      backgroundImage: FileImage(this._imageFile!),
-    );
   }
 }
